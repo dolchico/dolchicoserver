7b73a54c079d98045efa553e32c9d2f46af63e22
Akash Kulshrestha <akashkulshrestha7@gmail.com>
2025-07-19 17:44:24 +0530

    Updated OAuth route

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ import productRouter       from './routes/productRoute.js';
 import cartRouter          from './routes/cartRoute.js';
 import orderRouter         from './routes/orderRoute.js';
 import adminRouter         from './routes/adminRoute.js';
-import OAuthRouter          from './routes/OauthRoute.js';
+import OAuthRouter          from './routes/OAuthRoute.js';
 import authRoutes           from './routes/authRoute.js'; 
 
 import './config/passport-setup.js';
