80cc383071ac6f8d348e1f060da5eb7790161686
dolchico <dolchico8@gmail.com>
2025-09-25 15:55:13 +0530

    Update mailService.js to resolve OTP sending bug

--- services/mailService.js
+++ services/mailService.js
@@ -11,8 +11,8 @@ const OTP_EXPIRATION_MINUTES = 10; // OTP expiration time
 // --- Nodemailer Transporter Setup ---
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",   // Hostinger SMTP server
-  port: 465,                    // Use 465 for SSL or 587 for TLS
-  secure: true,                 // true for port 465, false for 587
+  port: process.env.SMTP_PORT,                    // Use 465 for SSL or 587 for TLS
+  secure: false,                 // true for port 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,     // Your full email address
     pass: process.env.EMAIL_PASSWORD, // Your email password or app password
